fdf2e57bb094a30861a948ba662e87e7733dbec0
dean <dean@swagmaster.ninja>
2021-07-15 13:17:49 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.image import image
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+
+class Background(Widget):
+    cloud_texture = ObjectProperty(None)
+    floor_texture = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # create textures
+        self.cloud_texture = image(source="cloud.png").texture
+        self.cloud_texture.wrap = 'repeat'
+        self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
+
+        self.floor_texture = image(source="floor.png").texture
+        self.floor_texture.wrap = 'repeat'
+        self.floor_texture.uvsize = (Window.width / self.floor_texture.width, -1)
