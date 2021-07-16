e29a77a619b2c9d8e2114db7194f2bc9223377ea
dean <dean@swagmaster.ninja>
2021-07-16 14:11:20 -0500

    fixed bad push

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Background(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # create textures
+        # Create textures
         self.cloud_texture = Image(source="cloud.png").texture
         self.cloud_texture.wrap = 'repeat'
         self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
@@ -21,3 +21,28 @@ class Background(Widget):
         self.floor_texture = Image(source="floor.png").texture
         self.floor_texture.wrap = 'repeat'
         self.floor_texture.uvsize = (Window.width / self.floor_texture.width, -1)
+
+    def on_size(self, *args):
+        self.cloud_texture.uvsize = (self.width / self.cloud_texture.width, -1)
+        self.floor_texture.uvsize = (self.width / self.floor_texture.width, -1)
+
+    def scroll_textures(self, time_passed):
+        # Update the uvpos of the texture
+        self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] - time_passed/2.0)%Window.width , self.cloud_texture.uvpos[1])
+        self.floor_texture.uvpos = ( (self.floor_texture.uvpos[0] - time_passed)%Window.width, self.floor_texture.uvpos[1])
+
+        # Redraw the texture
+        texture = self.property('cloud_texture')
+        texture.dispatch(self)
+
+        texture = self.property('floor_texture')
+        texture.dispatch(self)
+
+
+class MainApp(App):
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/60.)
+    pass
+
+
+MainApp().run()
